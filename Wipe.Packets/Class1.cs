a525c130e4220fcd61a389ae22039e5069100356
Liam Conlan <liam@officeirc.com>
2017-08-16 18:52:57 +0100

    Added Serializer to Wipe.Packets.

--- Wipe.Packets/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Wipe.Packets
-{
-    public class Class1
-    {
-    }
-}
---
+++ Wipe.Packets/Serializer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Wipe.Packets
+{
+    /// <summary>
+    /// Serializes and Deserializes Structs.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class Serializer<T>
+    {
+        public T Deserialize(byte[] raw)
+        {
+            return Deserialize(raw, 0);
+        }
+
+        public T Deserialize(byte[] raw, int position)
+        {
+            int size = Marshal.SizeOf<T>();
+
+            if (size > raw.Length)
+            {
+                return default(T);
+            }
+
+            IntPtr buffer = Marshal.AllocHGlobal(size);
+
+            Marshal.Copy(raw, position, buffer, size);
+
+            T obj = (T)Marshal.PtrToStructure<T>(buffer);
+
+            Marshal.FreeHGlobal(buffer);
+
+            return obj;
+        }
+
+        public byte[] Serialize(T item)
+        {
+            int size = Marshal.SizeOf<T>();
+
+            IntPtr buffer = Marshal.AllocHGlobal(size);
+
+            Marshal.StructureToPtr<T>(item, buffer, false);
+
+            byte[] raw = new byte[size];
+
+            Marshal.Copy(buffer, raw, 0, size);
+
+            Marshal.FreeHGlobal(buffer);
+
+            return raw;
+        }
+    }
+}
